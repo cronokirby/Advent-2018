2758d818b47836565ea538fc701d290cb9cab839
Lúcás Meier <cronokirby@gmail.com>
2018-12-05 16:25:19 +0100

    Make the solution really smol

--- 5.hs
+++ 5.hs
@@ -1,45 +1,22 @@
-import Control.Monad (guard)
-import Data.Char (isUpper, toLower, toUpper)
-import Data.Maybe (catMaybes)
+import Data.Char (ord, toLower)
 
 
-data PolymerType = Upper | Lower deriving (Eq, Show)
-
-data Unit = Unit PolymerType Char deriving (Eq, Show)
-
-opposite :: Unit -> Unit -> Bool
-opposite (Unit Upper c1) (Unit Lower c2) = c1 == c2
-opposite (Unit Lower c1) (Unit Upper c2) = c1 == c2
-opposite _ _                             = False
-
-ofType :: Char -> Unit -> Bool
-ofType c (Unit _ c1) = c == c1
-
-type Polymer = [Unit]
-
-readPolymer :: String -> Polymer
-readPolymer = map convert
-  where
-    convert c
-      | isUpper c = Unit Upper (toLower c)
-      | otherwise = Unit Lower c
-
-
-solve1 :: Polymer -> Int
+solve1 :: String -> Int
 solve1 ps = 
     length $ foldr go [] ps
   where
+    opposite c1 c2 = abs (ord c1 - ord c2) == 32
     go x []     = [x]
     go x l@(p:ps)
       | opposite x p = ps
       | otherwise    = x : l
 
-solve2 :: Polymer -> Int
-solve2 pl = minimum $ map (\c -> solve1 $ filter (not . ofType c) pl) ['a'..'z']
+solve2 :: String -> Int
+solve2 pl = minimum $ map (\c -> solve1 $ filter ((/= c) . toLower) pl) ['a'..'z']
 
 
 main :: IO ()
 main = do
-    input <- readPolymer <$> readFile "5.txt"
+    input <- readFile "5.txt"
     putStrLn ("Solution 1: " ++ show (solve1 input))
     putStrLn ("Solution 2: " ++ show (solve2 input))
