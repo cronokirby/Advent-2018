137be8781ed57ecd78a1666fad8bbf01db8011d7
Lúcás Meier <cronokirby@gmail.com>
2018-12-25 11:42:55 +0100

    Add full day 20 solution

--- 20.hs
+++ 20.hs
@@ -12,7 +12,7 @@ makeEdge p1 p2
     | otherwise = Edge p2 p1
 
 
-data Dir = North | South | West | East
+data Dir = North | South | West | East deriving (Show)
 
 readDir :: Char -> Dir
 readDir c = case c of
@@ -29,11 +29,11 @@ move dir = case dir of
     East  -> \(Pos x y) -> Pos (x + 1) y
     West  -> \(Pos x y) -> Pos (x - 1) y
 
-edgeTo :: Dir -> Pos -> Edge
-edgeTo dir pos = makeEdge pos (move dir pos)
+adjacent :: Pos -> [Pos]
+adjacent pos = map (`move` pos) [North, South, West, East]
 
 
-data RegTok = RegDir Dir | RegStart | RegEnd | RegOr | RegLPar | RegRPar
+data RegTok = RegDir Dir | RegStart | RegEnd | RegOr | RegLPar | RegRPar deriving (Show)
 
 readTok :: Char -> RegTok
 readTok c = case c of
@@ -47,4 +47,56 @@ readTok c = case c of
 
 type Path = [RegTok]
 
+readPath :: String -> Path
+readPath = map readTok
 
+
+pathEdges :: Path -> Set.Set Edge
+pathEdges path = go path (Pos 0 0) [] Set.empty
+  where
+    go [] _ _ set = set
+    go (RegStart:ts) pos stack set    = go ts pos stack set
+    go (RegEnd:ts) _ _ set            = set
+    go (RegLPar:ts) pos stack set     = go ts pos (pos:stack) set
+    go (RegOr:ts) _ stack@(pos:_) set = go ts pos stack set
+    go (RegRPar:ts) _ (pos:stack) set = go ts pos stack set
+    go ((RegDir d):ts) pos stack set  =
+        let nextPos = move d pos
+            set' = Set.insert (makeEdge pos nextPos) set
+        in go ts nextPos stack set'
+
+furthestRoom :: Set.Set Edge -> Int
+furthestRoom edges = go 0 Set.empty (Pos 0 0)
+  where
+    go n seen pos
+        | null neighbors = n
+        | otherwise      = 
+            maximum $ go (n + 1) (Set.insert pos seen) <$> neighbors
+      where
+        neighbors = 
+            filter ((`Set.member` edges) . makeEdge pos) 
+            . filter (`Set.notMember` seen) $ adjacent pos
+
+distances :: Set.Set Edge -> [Int]
+distances edges = go 0 Set.empty (Pos 0 0)
+  where
+    go n seen pos = 
+        let neighbors =
+                filter ((`Set.member` edges) . makeEdge pos)
+                . filter (`Set.notMember` seen) $ adjacent pos
+        in n : (go (n + 1) (Set.insert pos seen) =<< neighbors)
+
+
+solve1 :: Set.Set Edge -> Int
+solve1 = furthestRoom
+
+
+solve2 :: Set.Set Edge -> Int
+solve2 = length . filter (>= 1000) . distances
+
+
+main :: IO ()
+main = do
+    edges <- pathEdges . readPath <$> readFile "data/20.txt"
+    putStrLn ("Solution 1: " ++ show (solve1 edges))
+    putStrLn ("Solution 2: " ++ show (solve2 edges))
